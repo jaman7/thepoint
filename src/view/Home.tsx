87da6e97747a42c029bcd725796e59ba334fb3e9
jaman <krzysztofbalon123@gmail.com>
2024-07-19 14:25:26 +0200

    run

--- src/view/Home.tsx
+++ src/view/Home.tsx
@@ -3,59 +3,73 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
+import './Home.scss';
+
 interface IData {
-  name: string;
-  hair_color: string;
-  height: string;
+  name?: string;
+  hair_color?: string;
+  height?: string;
+  [name: string]: any;
 }
+
+interface IRestData {
+  count: number;
+  next: string;
+  previous: null;
+  results?: IData[];
+}
+
+const StyledTableRow = styled.tr<{ isActive: boolean }>`
+  color: ${({ isActive }) => (isActive ? 'red' : 'black')};
+`;
 
 const Home = () => {
   const [data, setData] = useState<IData[]>([]);
-
-  const [color, setColor] = useState<string>('blue');
-
-  const navigate = useNavigate();
+  const [clickedRow, setClickedRow] = useState<string | null>(null);
 
   const http = new HttpService();
 
   useEffect(() => {
-    http.get('https://swapi.dev/api/people/').then(myData => {
-      setData(
-        myData?.results?.map(item => {
-          const { name, hair_color, height } = item ?? {};
-          return { name, hair_color, height };
-        }) ?? []
-      );
-    });
+    const fetchData = async () => {
+      try {
+        const myData: IRestData = await http.get('https://swapi.dev/api/people/');
+        if (myData.results) {
+          const formattedData = myData.results.map(({ name, hair_color, height }) => ({ name, hair_color, height }));
+          setData(formattedData);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
   }, []);
 
-  const tableHeader = ['name', 'Height', 'Skin color'];
+  const tableHeader = ['Name', 'Height', 'Hair color'];
 
-  const changeColor = () => {
-    setColor(color => (color === 'blue' ? 'red' : 'blue'));
+  const changeColor = (name: string) => {
+    setClickedRow(prevName => (prevName === name ? null : name));
   };
 
   return (
-    <>
-      <table>
-        <thead>
-          <tr>
-            {tableHeader.map(header => (
-              <th key={header}>{header}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(item => (
-            <tr key={item.name}>
-              <td onClick={() => changeColor()}>{item?.name}</td>
-              <td>{item?.height}</td>
-              <td>{item?.hair_color}</td>
-            </tr>
+    <table className="table">
+      <thead>
+        <tr>
+          {tableHeader.map(header => (
+            <th key={header}>{header}</th>
           ))}
-        </tbody>
-      </table>
-    </>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(item => (
+          <tr key={item.name} className={clickedRow === item.name ? 'active-row' : ''} onClick={() => changeColor(item?.name ?? '')}>
+            <td>{item.name}</td>
+            <td>{item.height}</td>
+            <td>{item.hair_color}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
